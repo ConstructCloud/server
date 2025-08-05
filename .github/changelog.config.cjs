695e5eada3b257f8f67fa39d81b59608da84b694
jordanhoare <jordan.hoare@outlook.com>
2025-08-05 19:18:55 +1000

    a

--- .github/changelog.config.cjs
+++ .github/changelog.config.cjs
@@ -1,6 +1,15 @@
+// Only output the latest "release" block (i.e., changes since last stable),
+// with Conventional Commit groups, and no dates/compare links/headers.
 module.exports = {
+  releaseCount: 1, // <- just the latest block
   writerOpts: {
-    headerPartial: "## {{#if version}}{{version}}{{/if}}\n",
+    mainTemplate: `{{#each commitGroups}}
+### {{title}}
+{{#each commits}}- {{header}}
+{{/each}}
+{{/each}}`,
+    headerPartial: "",
+    footerPartial: "",
     linkCompare: false,
     linkReferences: false,
   },
